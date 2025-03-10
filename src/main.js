53f31d78aa97bba5f1e34c8cf6f914c8f92548bc
Gabe <gabeflavin@gmail.com>
2025-03-10 17:45:11 -0400

    add jQuery dependency, update animations, and modify HTML structure

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Lenis from 'lenis';
 import 'lenis/dist/lenis.css';
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import jQuery from 'jquery';
+const $ = jQuery;
 
 
 // Initialize Lenis
@@ -11,63 +13,101 @@ const lenis = new Lenis({
 
 
 
-// Create timeline for opening animation
-const tl = gsap.timeline();
-const splitText = new SplitType('.opening-copy');
+function initHero() {
 
-tl.from('.title', {
-  y: 100,
-  opacity: 0,
-  duration: 1,
-  ease: 'power3.out'
-})
-.from('.opening', {
-  opacity: 1,
-  duration: 1
-})
-.from(splitText.words, {
-  opacity: 0,
-  rotation: -40,
-  duration: .5,
-  stagger: 0.25,
-  ease: 'elastic.out(1,0.3)'
-})
-.from(splitText.words, {
-  opacity: 0,
-  scale: .8,
-  delay: 1,
-  duration: 2,
-  stagger: 0.25,
-  ease: 'power1.in'
-})
-.to('.opening', {
-  opacity: 0,
-  duration: 0,
-  delay: 1,
-  ease: 'power2.inOut',
-  onComplete: () => {
-    document.querySelector('.opening').style.display = 'none';
-  }
-})
-.to('.title', {
-  duration: 23,
-  scale: .85,
-  ease: 'power2.Out',
-  delay: -2
+  /*
+
+
+  ScrollTrigger for hero section
+
+
+  */
+  gsap.registerPlugin(ScrollTrigger);
+  gsap.to('.hero', {
+    scrollTrigger: {
+      trigger: '.hero',
+      start: 'top top',
+      end: 'bottom top',
+      scrub: true,
+      markers: false,
+    },
+    y: 600,
+    opacity: 0,
+    duration: 1
+  });
+
+
+  const heroVideo = document.querySelector('.hero video');
+  
+  // Create timeline for opening animation
+  const tl = gsap.timeline({ paused: true });
+  heroVideo.play();
+tl.play();
+  const splitText = new SplitType('.opening-copy');
+
+  tl.from(splitText.words, {
+    opacity: 0,
+    delay: 3,
+    rotation: -40,
+    duration: .5,
+    stagger: 0.25,
+    ease: 'elastic.out(1,0.3)'
+  })
+  .to('.opening', {
+    opacity: 0,
+    duration: 0,
+    delay: 2,
+    ease: 'power2.inOut',
+    onComplete: () => {
+      document.querySelector('.opening').style.display = 'none';
+    }
+  });
+}
+if ($('.hero')[0]) {
+  initHero();
+}
+
+ 
+
+
+/* 
+
+
+
+Pause video when out of viewport
+
+
+
+*/
+
+function isElementInViewport() {
+  const video = document.querySelector('video');
+ 
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          // Video is in the viewport, play it
+          video.play();
+
+          console.log('playing');
+        } else {
+          // Video is out of the viewport, pause it
+          video.pause();
+          console.log('paused');
+        }
+      });
+    }, { threshold: 0.5 }); // Adjust the threshold as needed
+  
+    observer.observe(video);
+}
+if ($('video')[0]) {
+  isElementInViewport();
+}
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  initHero();
 });
-
-// create a scrolltrigger animation for the hero section that will trigger when the user scrolls past the hero section
-gsap.registerPlugin(ScrollTrigger); // register the ScrollTrigger plugin    
-gsap.to('.hero', {
-  scrollTrigger: {
-    trigger: '.hero',
-    start: 'top top',
-    end: 'bottom top',
-    scrub: true,
-    markers: true,
-  },
-  y: 600,
-  opacity: 0,
-  duration: 1
-});
-
